5d24650db6f240c5b6c38beda39fba86391276a2
是66呀 <2652915018@qq.com>
2024-08-14 23:46:10 +0800

    添加环境变量

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+
+
 const nextConfig = {
     output: 'export',
-
-    assetPrefix: '/nextjs-test',
+    basePath: isProd ? '/nextjs-test' : '',
+    assetPrefix: isProd ? '/nextjs-test/' : '',
+    
     
    
 };
