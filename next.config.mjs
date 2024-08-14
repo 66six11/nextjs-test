c54fa12e20bd4c617a1c8a45015740cb47c3cabb
是66呀 <2652915018@qq.com>
2024-08-14 23:41:46 +0800

    添加assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/nextjs-test',
+
+    assetPrefix: '/nextjs-test',
+    
+   
 };
 
 export default nextConfig;
