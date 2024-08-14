980ac1dd7c4be9850963e51310853660f108fcbe
是66呀 <2652915018@qq.com>
2024-08-15 01:09:04 +0800

    调整ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,12 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="Zh-CN">
-    <head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>{metadata.title}</title>
-      <meta name="description" content={metadata.description} />
-    </head>
+    
       <body >{children}</body>
     </html>
   );
