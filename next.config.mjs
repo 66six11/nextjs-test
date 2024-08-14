9dba33ad0e113980aae0a989fef894c4b87ab697
是66呀 <2652915018@qq.com>
2024-08-15 00:15:57 +0800

    回退到上一个版本

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+
+
 const nextConfig = {
-    output: 'export',
+   
+    basePath: isProd ? '/nextjs-test' : '',
+    assetPrefix: isProd ? '/nextjs-test/' : '',
+    
+    
+   
 };
+
 
 export default nextConfig;
