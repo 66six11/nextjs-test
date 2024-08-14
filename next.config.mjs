3b4af4cb95d88ad81825a8e3b10ca5962b7193e4
是66呀 <2652915018@qq.com>
2024-08-14 23:54:49 +0800

    移除静态导出选项

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,13 @@ const isProd = process.env.NODE_ENV === 'production';
 
 
 const nextConfig = {
-    output: 'export',
+   
     basePath: isProd ? '/nextjs-test' : '',
     assetPrefix: isProd ? '/nextjs-test/' : '',
     
     
    
 };
+
 
 export default nextConfig;
